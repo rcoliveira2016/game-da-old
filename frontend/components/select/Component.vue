b5b197b62b284d9039ca20c2573bff11afdd0a88
Ramon Oliveira <roramon7@gmail.com>
2024-04-04 17:58:23 -0300

    feat: criação do componente de SelectCustom

---
+++ frontend/components/select/Component.vue
@@ -0,0 +1,37 @@
+<template>
+    <select v-model="modelValue" :required="props.required" :disabled="props.disabled" class="custom-select">
+        <option v-for="option in props.options" :key="option.value" :value="option.value">
+            {{ option.label }}
+        </option>
+    </select>
+</template>
+<script setup lang="ts">
+const modelValue = defineModel<string>();
+const props = defineProps({
+    options: {
+        type: Array as PropType<Array<{ value: string; label: string }>>,
+        required: true
+    },
+    required: {
+        type: Boolean,
+        default: false
+    },
+    disabled: {
+        type: Boolean,
+    }
+})
+</script>
+<style scoped>
+select {
+    padding: 0.2rem 0.4rem;
+    background-color: var(--color-background);    
+    color: var(--color-text);
+    border-color: var(--color-text);
+    border-radius: .2rem;
+}
+
+option {
+    background-color: var(--color-background);
+    color: var(--color-text);
+}
+</style>
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -3,19 +3,15 @@
     <header class="header-toolbar">
       <div class="justify-space-between">
         <div>
-          <label for="iaPlayer">Como Jogar: </label>
-          <select id="iaPlayer" v-model="iaPlayer">
-            <option value="">PvP</option>
-            <option value="O">X vs IA</option>
-            <option value="X">O vs IA</option>
-          </select>
+          <label for="iaPlayer" class="text-2">Como Jogar: </label>
+          <SelectComponent v-model="iaPlayer" :options="options" />
         </div>
         <div v-if="iaPlayer">
           AI:
           <JogoVelaIdentificadorJogador :jogador="iaPlayer" />
         </div>
       </div>
-      <div>
+      <div class="pym">
         <input type="checkbox" v-model="usarVoz"> usar comando de voz
         <div v-if="usarVoz"> usar comando de voz: "Jogar linha [1-3] coluna [1-3]"</div>
       </div>
@@ -29,6 +25,7 @@
 <script setup lang="ts">
 import JogoDaVelhaComponent from '@/components/jogo-vela/JogoDaVelhaComponent.vue';
 const { board, jogadorAtual, ganhador, iaPlayer, usarVoz, setarJogada, resetar, iniciar } = useManegerJogoDaVelha();
+const options = [{ value:"", label:"PvP" },{value:"O",label:"X vs IA"},{value:"X",label:"O vs IA"}]
 onMounted(() => {
   iniciar();
 })
